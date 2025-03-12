0e2afc38d97557af0298a554e96916375311ac03
Ilyas <ilyasboudhaine1@gmail.com>
2025-03-12 12:15:23 +0000

    UI improvements

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -27,21 +27,25 @@ export default {
 </script>
 
 <template>
-  <div class="card" style="width: 16rem">
-    <img :src="produit.Image" class="card-img-top img-thumbnail" alt="..." />
-      <div class="card-body">
-    <h5 class="card-title">{{ produit.nom }}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{ produit.Disponible ? "Disponible" : "En rupture" }}, {{ produit.prix}}$</h6>
-    <button href="" class="btn" :class="produit.Disponible ? 'btn-outline-success' : 'btn-danger'" 
-    @click="add(produit.id)"
-    >Add to cart</button>
-  </div>
+  <div class="card">
+    <img :src="produit.Image" class="card-img-top" alt="Image du produit" />
+    <div class="card-body">
+      <h5 class="card-title">{{ produit.nom }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">
+        <span :class="produit.Disponible ? 'text-success' : 'text-danger'">
+          {{ produit.Disponible ? "Disponible" : "En rupture" }}
+        </span>, 
+        <span class="fw-bold">{{ produit.prix }}$</span>
+      </h6>
+      <button 
+        class="btn" 
+        :class="produit.Disponible ? 'btn-outline-success' : 'btn-danger'" 
+        @click="add(produit.id)"
+        :disabled="!produit.Disponible"
+      >
+        {{ produit.Disponible ? 'Add to cart' : 'En rupture' }}
+      </button>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.btn-danger:hover{
-    cursor: not-allowed;
-    background-color: #dc3545;
-}
-</style>
